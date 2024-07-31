a2e39d038ffb8af6a889ad43277a62ca09014f21
BrandonW-cmd <809bsw@gmail.com>
2024-07-31 18:50:18 +0000

    madlibs assignment

---
+++ script.js
@@ -0,0 +1,10 @@
+let verb1 = "walking"
+let noun1 = "criminal"
+let number = "12"
+let enumber = 18-6
+let jbnumber = prompt("choose a number 1-10")
+
+
+
+
+console.log("I was " + verb1 + " with " + number + " jellybeans" + " and that was when i saw a criminal " + enumber + " feet away from me." + " The criminal stole" + jbnumber)
