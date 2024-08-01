e1d488092674f83fb022f617e2dbfde5ffe96e40
BrandonW-cmd <809bsw@gmail.com>
2024-08-01 14:52:30 +0000

    finished

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ let verb1 = "walking"
 let noun1 = "criminal"
 let number = "12"
 let enumber = 18-6
+let adjective = " tasty"
 let jbnumber = prompt("choose a number 1-10")
 
 
 
 
-console.log("I was " + verb1 + " with " + number + " jellybeans" + " and that was when i saw a criminal " + enumber + " feet away from me." + " The criminal stole" + jbnumber)
+console.log("I was " + verb1 + " with " + number + adjective + " jellybeans" + " and that was when i saw a criminal " + enumber + " feet away from me." + " The criminal stole " + jbnumber + " from me but that was when he got scared " + " because he saw a cop and when the cop saw what he did he got arrested and went to jail." )
